afedcd766c7ac923a9ef5c2d47d3e9fbe7a83a8b
Kishan S <kishan.shanthakumar@gmail.com>
2023-04-24 18:55:48 +0100

    added search for bp, valid for addr

--- Computer Architecture/RV64IMFD/riscv/bp.sv
+++ Computer Architecture/RV64IMFD/riscv/bp.sv
@@ -1,7 +1,9 @@
 module bp( input logic [31:0] addr,
             input logic train,
             output logic valid,
-            inout logic [31:0] paddr
+            output logic [31:0] paddr,
+            input logic [31:0] taddr,
+            input clk, rst
 );
 
 struct {
@@ -9,7 +11,44 @@ struct {
     logic [31:0] pred_address;
     logic [1:0] counter;
     logic valid;
+    logic match;
 } btb_ele;
 
 btb_ele [2047:0] btb;
+logic present;
+
+always_comb
+begin 
+    present = '0;
+    for (i = 0; i < 2048; i = i + 1)
+    begin
+        present |= btb_ele[i].match;
+    end
+end
+
+always_ff @(posedge clk, negedge rst)
+begin
+    if (!rst)
+        btb <= {2048{32'b0, 32'b0, 2'b0, 1'b0}};
+    else
+        if(train)
+        begin
+            for(integer i = 0; i < 2048; i++)
+            begin
+                if (btb[i].match)
+                begin
+                    if (btb[i].counter != 0)
+                    begin
+                        btb[i].counter <= btb[i][counter] - 1;
+                    end
+                end
+            end
+        end
+        for(integer i = 0; i < 2048; i++)
+        begin
+            btb[i].match <= addr == taddr;
+            btb[i].valid <= addr == addr;
+        end
+end
+
 endmodule
