364ffa356ce9e3cce954dd21c87f7ba657b91873
Kishan S <kishan.shanthakumar@gmail.com>
2023-04-25 19:21:40 +0000

    TODO: added as comments

--- Computer Architecture/RV64IMFD/riscv/bp.sv
+++ Computer Architecture/RV64IMFD/riscv/bp.sv
@@ -2,11 +2,17 @@ module bp( input logic [31:0] addr,
             input logic train,
             output logic valid,
             output logic [31:0] paddr,
-            input logic [31:0] taddr,
+            input logic [31:0] t_addr,
+            input logic [31:0] t_paddr,
             input clk, rst
 );
 
-struct {
+logic [10:0] index;
+logic enc_valid;
+
+enc_n #(11) u1(index, enc_valid, validn);
+
+typedef struct {
     logic [31:0] address;
     logic [31:0] pred_address;
     logic [1:0] counter;
@@ -14,41 +20,80 @@ struct {
     logic match;
 } btb_ele;
 
-btb_ele [2047:0] btb;
+btb_ele btb [2047:0];
 logic present;
+logic [2047:0] validn;
+
+// Prediction phase
+always_comb
+begin
+    for(integer i = 0; i < 2048; i++)
+    begin
+        validn[i] = btb[i].valid;
+    end
+end
 
 always_comb
-begin 
-    present = '0;
-    for (i = 0; i < 2048; i = i + 1)
+begin
+    valid = 0;
+    for(integer i = 0; i < 2048; i++)
     begin
-        present |= btb_ele[i].match;
+        valid |= btb[i].valid;
+        btb[i].valid = btb[i].address == addr;
+    end
+end
+
+assign paddr = (enc_valid == 1) ? btb[index].pred_address : 'z;
+
+//Training phase
+always_comb
+begin 
+    present = 0;
+    for (integer i = 0; i < 2048; i = i + 1)
+    begin
+        present |= btb[i].match;
+    end
+    for(integer i = 0; i < 2048; i++)
+    begin
+        btb[i].match = btb[i].address == t_addr;
     end
 end
 
 always_ff @(posedge clk, negedge rst)
 begin
     if (!rst)
-        btb <= {2048{32'b0, 32'b0, 2'b0, 1'b0}};
+    begin
+        for(integer i = 0; i < 2048; i++)
+        begin
+            btb[i].address <= 32'b0;
+            btb[i].pred_address <= 32'b0;
+            btb[i].counter <= 2'b0;
+        end
+    end
     else
+    begin
         if(train)
         begin
-            for(integer i = 0; i < 2048; i++)
+            if (present)
             begin
-                if (btb[i].match)
+                for(integer i = 0; i < 2048; i++)
                 begin
-                    if (btb[i].counter != 0)
+                    if (btb[i].match)
                     begin
-                        btb[i].counter <= btb[i][counter] - 1;
+                        if (btb[i].counter != 0)
+                        begin
+                            btb[i].counter <= btb[i].counter - 1;
+                        end
                     end
                 end
             end
+            else
+            begin
+                // Inserting new addresses
+                // Potential replacement for existing addresses
+            end
         end
-        for(integer i = 0; i < 2048; i++)
-        begin
-            btb[i].match <= addr == taddr;
-            btb[i].valid <= addr == addr;
-        end
+    end
 end
 
 endmodule
