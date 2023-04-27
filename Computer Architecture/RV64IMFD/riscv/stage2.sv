5d2c95a0fdb1efb27accad15bbb5864dc78ed671
Kishan Shanthakumar (ks8n22) <ks8n22@soton.ac.uk>
2023-04-27 13:22:43 +0100

    Added immediate output from decoder, eu_type

--- Computer Architecture/RV64IMFD/riscv/stage2.sv
+++ Computer Architecture/RV64IMFD/riscv/stage2.sv
@@ -1,5 +1,8 @@
 module stage2(input logic [31:0] instr,
-            output [4:0] r1_addr, r2_addr, w1_addr,
-            // output eu_type,
+            output [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
+            output [3:0] reg_control,
+            output [20:0] imm,
+            output [1:0] eu_type,
             input clk, rst);
+
 endmodule
