882e98254875fb1d69ed88cff170f28943b83e97
Kishan S <kishan.shanthakumar@gmail.com>
2023-05-03 01:27:56 +0000

    corrected all errors, reduced for loop usage

--- Computer Architecture/RV64IMFD/riscv/bp.sv
+++ Computer Architecture/RV64IMFD/riscv/bp.sv
@@ -1,4 +1,4 @@
-module bp #(parameter N)
+module bp #(parameter N = 11)
             (input logic [31:0] addr,
             output logic hit, taken,
             output logic [31:0] paddr,
@@ -8,15 +8,13 @@ module bp #(parameter N)
             input clk, rst
 );
 
-logic [N-1:0] index_free, index;
+logic [N-1:0] index_free, index, index_match;
 logic enc_valid, gfree;
-genvar i, j;
+genvar j;
 logic present;
 logic [((2**N)-1):0] validn;
 logic [((2**N)-1):0] freen;
-
-enc_n #(N) u1(index, enc_valid, validn);
-enc_n #(N) u2(index_free, gfree, freen);
+logic [((2**N)-1):0] match;
 
 typedef struct {
     logic [31:0] address;
@@ -24,7 +22,6 @@ typedef struct {
     logic [1:0] counter;
     logic [1:0] prev_counter;
     logic valid;
-    logic match;
     logic free;
 } btb_ele;
 
@@ -34,114 +31,90 @@ btb_ele btb [((2**N)-1):0];
 generate
 for(j = 0; j < 2**N; j++)
 begin : predictor
-    assign validn[j] = btb[j].valid;
+    assign validn[j] =(btb[j].address == addr) ? 1 : 0;
     assign freen[j] = btb[j].free;
     assign btb[j].valid = (btb[j].address == addr) ? 1 : 0;
 end
 endgenerate
 
+enc_n #(N) u1(index, enc_valid, validn);
+enc_n #(N) u2(index_free, gfree, freen);
+enc_n #(N) u3(index_match, present, match);
+
 assign paddr = btb[index].pred_address;
 assign taken = (btb[index].counter > 1) ? 1 : 0;
-assign hit = |validn;
+assign hit = enc_valid;
 
 //misprediction phase
-always_comb
-begin 
-    present = 0;
-    for (integer k = 0; k < 2**N; k = k + 1)
-    begin
-        present |= btb[k].match;
-    end
-end
-
 generate
 for(j = 0; j < 2**N; j++)
 begin : matcher
-    assign btb[j].match = (btb[j].address == t_addr) ? 1 : 0;
+    assign match[j] = (btb[j].address == t_addr) ? 1 : 0;
 end
 endgenerate
 
-generate
-for(i = 0; i < 2**N; i++)
-begin : training
-    always_ff @(posedge clk, negedge rst)
+always_ff @(posedge clk, negedge rst)
+begin
+    if (!rst)
     begin
-        if (!rst)
+        for(int i = 0; i < 2**N; i++)
         begin
-            btb[i].address <= 32'b0;
-            btb[i].pred_address <= 32'b0;
-            btb[i].counter <= 2'b0;
-            btb[i].prev_counter <= 2'b0;
-            btb[i].free <= 1'b0;
+            btb[i].address <= 0;
+            btb[i].pred_address <= 0;
+            btb[i].counter <= 0;
+            btb[i].prev_counter <= 0;
+            btb[i].free <= 1;
         end
-        else
+    end
+    else
+    begin
+        if (gfree & !present & mispred)
         begin
-            if (mispred)
+            btb[index_free].address <= t_addr;
+            btb[index_free].pred_address <= tp_addr;
+            btb[index_free].free <= 0;
+        end
+        if (hit)
+        begin
+            btb[index].prev_counter <= btb[index].counter;
+            if (btb[index].counter == 0)
             begin
-                if (present)
-                begin
-                    if (btb[i].match == 1)
-                    begin
-                        if (btb[i].prev_counter == 0)
-                        begin
-                            btb[i].counter <= 1;
-                        end
-                        else if (btb[i].prev_counter == 1)
-                        begin
-                            btb[i].counter <= 2;
-                        end
-                        else if (btb[i].prev_counter == 2)
-                        begin
-                            btb[i].counter <= 1;
-                        end
-                        else
-                        begin
-                            btb[i].counter <= 2;
-                        end
-                    end
-                end
-                else
-                begin
-                    if (gfree == 1)
-                    begin
-                        if (i == index_free)
-                        begin
-                            btb[i].address <= t_addr;
-                            btb[i].pred_address <= tp_addr;
-                            btb[i].free <= 0;
-                        end
-                    end
-                end
+                btb[index].counter <= 0;
+            end
+            else if (btb[index].counter == 1)
+            begin
+                btb[index].counter <= 0;
+            end
+            else if (btb[index].prev_counter == 2)
+            begin
+                btb[index].counter <= 3;
             end
             else
             begin
-                if (hit == 1)
-                begin
-                    if (btb[i].valid == 1)
-                    begin
-                        btb[i].prev_counter <= btb[i].counter;
-                        if (btb[i].counter == 0)
-                        begin
-                            btb[i].counter <= 0;
-                        end
-                        else if (btb[i].counter == 1)
-                        begin
-                            btb[i].counter <= 0;
-                        end
-                        else if (btb[i].prev_counter == 2)
-                        begin
-                            btb[i].counter <= 3;
-                        end
-                        else
-                        begin
-                            btb[i].counter <= 3;
-                        end
-                    end
-                end
+                btb[index].counter <= 3;
+            end
+        end
+        if (mispred & present)
+        begin
+            btb[index_match].prev_counter <= btb[index_match].counter;
+            if (btb[index_match].counter == 0)
+            begin
+                btb[index_match].counter <= 0;
+            end
+            else if (btb[index_match].counter == 1)
+            begin
+                btb[index_match].counter <= 0;
+            end
+            else if (btb[index_match].prev_counter == 2)
+            begin
+                btb[index_match].counter <= 3;
+            end
+            else
+            begin
+                btb[index_match].counter <= 3;
             end
         end
     end
 end
-endgenerate
 
 endmodule
