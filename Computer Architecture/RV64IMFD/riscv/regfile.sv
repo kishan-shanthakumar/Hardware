d843b86429c4c9dfcc35ed8667fda35c9d075fe5
Kishan S <kishan.shanthakumar@gmail.com>
2023-05-07 04:15:47 +0000

    added fp registers

--- Computer Architecture/RV64IMFD/riscv/regfile.sv
+++ Computer Architecture/RV64IMFD/riscv/regfile.sv
@@ -1,9 +1,13 @@
+// 32 base XLEN registers
+// 32 fp 64 bit registers since D is added in the ISA
+
 module regfile (input [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
                 input [31:0] inp_data,
                 output [31:0] r1_data, r2_data, r3_addr,
                 input clk, rst, write);
 
-logic [31:0] base_reg [0:31];
+logic [63:0] base_reg [0:31];
+logic [63:0] fp_reg [0:31];
 
 always_comb
 begin
