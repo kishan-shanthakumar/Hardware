285458df87e8555782d8deeb9b04c7a027972657
Kishan S <kishan.shanthakumar@gmail.com>
2023-06-04 03:19:14 +0000

    progress of decode stage

--- Computer Architecture/RV64IMFD/riscv/stage2.sv
+++ Computer Architecture/RV64IMFD/riscv/stage2.sv
@@ -5,10 +5,30 @@
 // Branch/Jump unit
 
 module stage2(input logic [31:0] instr,
+            input logic ready,
+            input logic pipeline_valid,
             output [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
-            output [3:0] reg_control,
+            output [3:0] reg_write,
             output [20:0] imm,
             output [1:0] eu_type,
-            input clk, rst);
+            input logic clk, rst);
+
+enum logic [2:0] { basic, muldiv, jumpbranch } eu; // Basic Arithmetic, Mul-Div, Jump-Branch
+
+logic [31:0] instruction;
+
+always_ff @(posedge clk, negedge rst)
+begin
+    if (!rst)
+        instruction <= 32'h13;
+    else if (ready)
+        instruction <= instr;
+end
+
+always_comb
+begin
+    case(instruction[1:0])
+    endcase
+end
 
 endmodule
