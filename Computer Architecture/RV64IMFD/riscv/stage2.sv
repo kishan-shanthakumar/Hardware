15753436ed1c699525ec0667c970d77861a59771
Kishan S <kishan.shanthakumar@gmail.com>
2023-08-04 05:21:01 +0100

    added fp select for regfile

--- Computer Architecture/RV64IMFD/riscv/stage2.sv
+++ Computer Architecture/RV64IMFD/riscv/stage2.sv
@@ -7,10 +7,11 @@
 module stage2(input logic [31:0] instr,
             input logic ready,
             input logic pipeline_valid,
-            output [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
-            output [3:0] reg_write,
-            output [20:0] imm,
-            output [1:0] eu_type,
+            output logic [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
+            output logic [3:0] reg_write,
+            output logic [20:0] imm,
+            output logic [1:0] eu_type,
+            output logic sel_i_f,
             input logic clk, rst);
 
 enum logic [2:0] { basic, muldiv, jumpbranch } eu; // Basic Arithmetic, Mul-Div, Jump-Branch
