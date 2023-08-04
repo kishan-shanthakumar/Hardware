365cf05e0a63950df3a7c28b2aa63ce5e98d0144
Kishan S <kishan.shanthakumar@gmail.com>
2023-08-04 05:19:53 +0100

    Including fp in the regfile ops

--- Computer Architecture/RV64IMFD/riscv/regfile.sv
+++ Computer Architecture/RV64IMFD/riscv/regfile.sv
@@ -1,19 +1,20 @@
 // 32 base XLEN registers
 // 32 fp 64 bit registers since D is added in the ISA
 
-module regfile (input [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
-                input [31:0] inp_data,
-                output [31:0] r1_data, r2_data, r3_addr,
-                input clk, rst, write);
+module regfile (input logic [4:0] r1_addr, r2_addr, r3_addr, w1_addr,
+                input logic [63:0] inp_data,
+                output logic [63:0] r1_data, r2_data, r3_addr,
+                input logic sel_i_f,
+                input logic clk, rst, write);
 
 logic [63:0] base_reg [0:31];
 logic [63:0] fp_reg [0:31];
 
 always_comb
 begin
-    r1_data = base_reg[r1_addr];
-    r2_data = base_reg[r2_addr];
-    r3_data = base_reg[r3_addr];
+    r1_data = (sel_i_f == 1'b1) ? fp_reg[r1_addr] : base_reg[r1_addr];
+    r2_data = (sel_i_f == 1'b1) ? fp_reg[r2_addr] : base_reg[r2_addr];
+    r3_data = (sel_i_f == 1'b1) ? fp_reg[r3_addr] : base_reg[r3_addr];
 end
 
 always_ff @(posedge clk, negedge rst)
@@ -23,10 +24,16 @@ begin
         for(integer i = 0; i < 32; i++)
         begin
             base_reg[i] <= 0;
+            fp_reg[i] <= 0;
         end
     end
     else if(write)
-        base_reg[w1_addr] <= inp_data;
+    begin
+        if(sel_i_f == 1'b1)
+            fp_reg[w1_addr] <= inp;
+        else
+            base_reg[w1_addr] <= inp_data;
+    end
 end
 
 endmodule
