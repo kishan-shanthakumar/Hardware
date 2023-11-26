a6fca3fce2d913ec0b4a986f8c5736250bba7095
Kishan S <kishan.shanthakumar@gmail.com>
2023-11-26 02:11:04 +0000

    minor fix

--- Computer Architecture/RV64IMFD/riscv/stage2.sv
+++ Computer Architecture/RV64IMFD/riscv/stage2.sv
@@ -1,7 +1,7 @@
 module stage2(
     input logic clk, n_reset,
-    input [47:0] pc,
-    input [31:0] instr,
+    input logic [47:0] pc,
+    input logic [31:0] instr,
     output logic [] type_op_mem,
     output logic [] op_funct_dec,
     output logic [4:0] rs1_dec, rs2_dec, rs3_dec, rd_dec,
