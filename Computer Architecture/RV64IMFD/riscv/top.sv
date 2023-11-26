f9476bc1332a4be5bf7c6879149900d81bfcbd48
Kishan S <kishan.shanthakumar@gmail.com>
2023-11-26 01:24:54 +0000

    top port definition

---
+++ Computer Architecture/RV64IMFD/riscv/top.sv
@@ -0,0 +1,17 @@
+module riscv (
+    parameter bht_size = 256,
+    parameter num_occurences = 4,
+    parameter ras_size = 8
+) (
+    input logic clk, n_reset,
+    input logic [31:0] instr,
+    input logic i_ready,
+    output logic [47:0] pc,
+    input logic [63:0] mem_data_mem_in,
+    input logic d_ready,
+    output logic [47:0] mem_addr_mem,
+    output logic we_rd_mem,
+    output logic [63:0] mem_data_mem_out
+)
+
+endmodule
