8e61ada6846a659df8b2fd3b1106425743ab95ff
Kishan S <kishan.shanthakumar@gmail.com>
2023-12-03 23:06:42 +0000

    minor cleanup

--- Computer Architecture/RV64IMFD/riscv_tb/sim.py
+++ Computer Architecture/RV64IMFD/riscv_tb/sim.py
@@ -1,8 +1,7 @@
-from pyyaml import yaml
+import yaml
 import sys
 
-mod = sys.argv(0)
-tool = sys.argv(1)
-di = yaml.safeload('module_dependency.yaml')
+[mod, tool] = sys.argv[1:]
+di = yaml.safe_load('module_dependency.yaml')
 
 outfile = 'file_list.txt'
