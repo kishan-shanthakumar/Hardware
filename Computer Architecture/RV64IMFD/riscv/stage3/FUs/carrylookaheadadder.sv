286138294b5d58436ccfe88f61a5c27730a48c98
Kishan S <kishan.shanthakumar@gmail.com>
2023-12-06 21:39:18 +0000

    minor fix and tb for cla

--- Computer Architecture/RV64IMFD/riscv/stage3/FUs/carrylookaheadadder.sv
+++ Computer Architecture/RV64IMFD/riscv/stage3/FUs/carrylookaheadadder.sv
@@ -1,5 +1,5 @@
 module carrylookaheadadder #(
-    parameter N = 32
+    parameter N = 64
 ) (
     input logic [N-1:0] a,b,
     output logic [N:0] out
@@ -7,34 +7,34 @@ module carrylookaheadadder #(
 
 logic [N-1:0] gen, prop;
 logic [N:0] carry;
+logic tempj;
+logic tempk;
 
 always_comb begin : cla
     carry = '0;
     for(int i = 0; i < N; i++)
-    {
+    begin
         gen[i] = a[i] & b[i];
         prop[i] = a[i] | b[i];
-    }
+    end
     for(int i = 1; i < N; i++)
-    {
-        logic tempj;
+    begin
         tempj = gen[i-1];
         for(int j = 0; j < i; j++)
-        {
-            logic tempk;
+        begin
             tempk = (j==0) ? carry[0] : gen[j-1];
             for(int k = j; k < i; k++)
-            {
+            begin
                 tempk = tempk & prop[k];
-            }
+            end
             tempj = tempj | tempk;
-        }
+        end
         carry[i] = tempj;
-    }
+    end
     for(int i = 0; i < N; i++)
-    {
+    begin
         out[i] = a[i] ^ b[i] ^ carry[i];
-    }
+    end
     out[N] = carry[N];
 end
 
