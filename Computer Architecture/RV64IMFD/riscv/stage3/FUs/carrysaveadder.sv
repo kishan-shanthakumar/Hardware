8bbaaabcd5d05bb373860896da8b46645459fb7c
Kishan S <kishan.shanthakumar@gmail.com>
2023-12-06 22:30:41 +0000

    minor fix

--- Computer Architecture/RV64IMFD/riscv/stage3/FUs/carrysaveadder.sv
+++ Computer Architecture/RV64IMFD/riscv/stage3/FUs/carrysaveadder.sv
@@ -7,11 +7,12 @@ module carrysaveadder #(
 
 always_comb
 begin
+    carry[0] = 0;
     for(int i = 0; i < N-1; i++)
-    {
+    begin
         sum[i] = a[i] ^ b[i] ^ c[i];
         carry[i+1] = ((a[i] ^ b[i]) & c[i]) | (a[i] & b[i]);
-    }
+    end
     sum[N-1] = a[N-1] ^ b[N-1] ^ c[N-1];
 end
 
